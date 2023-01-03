f5d37ead56ea2e949b3332637edc88d4ff93063d
HagerSherif <93436199+HagerSherif@users.noreply.github.com>
2023-01-03 16:22:04 +0200

    new_update

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ import cv2
 import numpy as np 
 import base64
 import io
-import PIL.Image as Image
+import PIL.Image as Pimage
 import base64
-from Fourier import Fourier2d
+from Fourier import Image
 import json
 
 
 
 app = Flask(__name__)
  
-first_image=Fourier2d()
-second_image=Fourier2d()
+first_image=Image()
+second_image=Image()
 
 
 def encode_plt(file_name1):
-    im= Image.open(file_name1)  
+    im= Pimage.open(file_name1)  
     data = io.BytesIO()
     im.save(data, "JPEG")
     encoded_img_data = base64.b64encode(data.getvalue())
@@ -31,22 +31,37 @@ def encode_plt(file_name1):
     return encoded_img_data  
 
 
-
 def decode_img(file,filename):
     npimg = np.fromstring(file, np.uint8)
     img = cv2.imdecode(npimg,cv2.IMREAD_COLOR)
-    img = Image.fromarray(img.astype("uint8"))
+    img = Pimage.fromarray(img.astype("uint8"))
     rawBytes = io.BytesIO()
     img.save(rawBytes, "JPEG")
     rawBytes.seek(0)
     img_base64 = base64.b64encode(rawBytes.read()) 
     with open("image.jpg", "wb") as fh:
-        fh.write(base64.decodebytes(img_base64)) 
-    
+        fh.write(base64.decodebytes(img_base64))  
     img = skimage.io.imread("image.jpg") 
     cv2.imwrite(filename, img) 
     original_image = cv2.imread(filename) 
+
     return original_image         
+
+
+def save_magnitude_phase_plts(image,image_file_name1,image_file_name2):
+    fig = plt.figure(figsize=(5, 5))
+    plt.imshow(np.log(image.fourier_transformed_image_magnitude))
+    plt.savefig(image_file_name1)
+    plt.imshow(image.fourier_transformed_image_phase)
+    plt.savefig(image_file_name2)
+
+def save_reconstructed_image(reconstructed):
+    fig = plt.figure(figsize=(4, 4))
+    plt.imshow(np.abs(reconstructed), cmap='gray')
+    plt.savefig('reconstructed.jpg')
+
+
+
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -61,11 +76,11 @@ def index():
             output = request.get_json()
             co = json.loads(output)
 
-            first_image.reconstruct(second_image,co['Mag_rectangle_x: ' ]
+            reconstructed=first_image.reconstruct(second_image,co['Mag_rectangle_x: ' ]
             ,co['Mag_rectangle_y: '],co['Mag_rectangle_width: ' ],co['Mag_rectangle_height: '],
-            co['Phase_rectangle_x: '],co['Phase_rectangle_y: '],co['Phase_rectangle_width: '],co[ 'Phase_rectangle_height: ' ])
+            co['Phase_rectangle_x: '],co['Phase_rectangle_y: '],co['Phase_rectangle_width: '],co[ 'Phase_rectangle_height: ' ],co['mode:'])
             
-            print(co)
+            save_reconstructed_image(reconstructed)
             encoded_img_data1=encode_plt("reconstructed.jpg")
             return jsonify({'status':str(encoded_img_data1)})
 
@@ -75,14 +90,14 @@ def index():
         
         if name==str(1):
             original_image1=decode_img(file,'image1.jpg')  
-            first_image=Fourier2d(rgb2gray(original_image1),400,750,60)
-            first_image.save_plts("mag1.jpg","phase1.jpg")
+            first_image=Image(rgb2gray(original_image1),438,789,97)
+            save_magnitude_phase_plts(first_image,"mag1.jpg","phase1.jpg")
             encoded_img_data1=encode_plt("mag1.jpg")
             encoded_img_data2=encode_plt("phase1.jpg")
         else:
             original_image2=decode_img(file,'image2.jpg') 
-            second_image=Fourier2d(rgb2gray(original_image2),400,750,400)
-            second_image.save_plts("mag2.jpg","phase2.jpg")
+            second_image=Image(rgb2gray(original_image2),438,789,436)
+            save_magnitude_phase_plts(second_image,"mag2.jpg","phase2.jpg")
             encoded_img_data1=encode_plt("mag2.jpg")
             encoded_img_data2=encode_plt("phase2.jpg")      
 
